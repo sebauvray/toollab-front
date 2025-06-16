3162b355ca50425cd7f0bbd6cbf499fc81759361
Younes SERRA <yserra@corp.free.fr>
2025-06-16 17:19:20 +0200

    fix public.apiUrl

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,7 +11,7 @@ export default defineNuxtConfig({
     ],
     runtimeConfig: {
         public: {
-            apiUrl: process.env.NUXT_PUBLIC_API_URL || 'http://localhost'
+            apiUrl: process.env.NUXT_PUBLIC_API_URL || 'http://localhost:8000'
         }
     },
     app: {
