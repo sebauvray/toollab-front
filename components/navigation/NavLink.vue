46fa8afb47094954edb34299bfc428eeea43531b
Redha El hanti <relhanti@gmail.com>
2025-06-18 03:26:44 +0200

    feat: sidebar

--- components/navigation/NavLink.vue
+++ components/navigation/NavLink.vue
@@ -12,6 +12,10 @@ const props = defineProps({
   text: {
     type: String,
     required: true
+  },
+  collapsed: {
+    type: Boolean,
+    default: false
   }
 })
 
@@ -27,19 +31,24 @@ const isActive = computed(() => {
 <template>
   <NuxtLink
       :to="to"
-      class="inline-flex items-center h-[3.75rem]"
+      class="inline-flex items-center h-[3.75rem] relative group"
       :class="[isActive ? 'text-default' : 'text-placeholder hover:text-default']"
   >
     <span
-        class="w-1.5 bg-default rounded-r-xl h-full mr-10"
+        class="w-1.5 bg-default rounded-r-xl h-full absolute left-0"
         :class="[isActive ? 'bg-default' : 'bg-white']"
     ></span>
-    <p class="inline-flex items-center justify-center">
-      <span class="inline-flex items-center justify-center gap-x-4 w-fit">
+    <p class="inline-flex items-center w-full"
+       :class="collapsed ? 'justify-center px-6' : 'justify-start pl-12 pr-6'">
+      <span class="inline-flex items-center gap-x-4">
         <component :is="icon" class="size-7" />
-        <span class="text-lg leading-5 font-medium">{{ text }}</span>
+        <span v-if="!collapsed" class="text-lg leading-5 font-medium">{{ text }}</span>
       </span>
     </p>
+
+    <div v-if="collapsed"
+         class="absolute left-full ml-2 px-2 py-1 bg-gray-800 text-white text-sm rounded opacity-0 pointer-events-none group-hover:opacity-100 whitespace-nowrap z-50 transition-opacity">
+      {{ text }}
+    </div>
   </NuxtLink>
 </template>
-
